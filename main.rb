aac8f0e08d14e964763d87eafde673eae98632f4
Skyler Sidner <malevolentdragon@gmail.com>
2015-02-12 14:58:36 -0600

    Small renaming fixes; added back button to product confirmation;

--- main.rb
+++ main.rb
@@ -23,31 +23,42 @@ get "/locations" do
   erb :locations
 end
 
-get "/l_create" do
-  erb :"locations/l_create"
-end
-
-get "/c_create" do
-  erb :"categories/c_create"
-end
-
-get "/p_new" do
-  erb :"products/p_new"
-end
 
 get "/l_index" do
   @all = Location.all
   erb :"locations/l_index"
 end
 
+
 get "/c_index" do
   @all = Category.all
   erb :"categories/c_index"
 end
 
+get "/c_new" do
+  erb :"categories/c_new"
+end
+
+get "/c_confirm" do
+  @genre = params["genre"]
+  erb :"categories/c_confirm"
+end
+
+get "/c_create" do
+  @c = Category.new(params)
+  @c.insert
+  @all = [@c]
+  erb :"categories/c_create"
+end
+
+
 get "/p_index" do
   @all = Product.all
   erb :"/products/p_index"
+end
+
+get "/p_new" do
+  erb :"products/p_new"
 end
 
 get "/p_confirm" do
